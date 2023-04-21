4d5d2c83ff04a205bf6a632cd87fd96af5a6756c
Box <forkome@gmail.com>
2023-04-21 15:53:44 +0800

    Update

--- src/CarSearch/bases/views.py
+++ src/CarSearch/bases/views.py
@@ -8,9 +8,10 @@ from car.models import Car
 @login_required
 def index(request):
     cursor = connection.cursor()
-    sql = """SELECT COUNT(*) FROM car_car a LEFT OUTER JOIN car_car2 b ON a.CARNO = b.CARNO WHERE (b.CARNO IS NULL OR a.FINDMODE <> b.FINDMODE)"""
-    cursor.execute(sql)
-    row_count = format(cursor.fetchone()[0], ',')
+    sql = """SELECT a.CARNO, a.CARTYPE, a.CARCOLOR, a.FINDMODE, a.ADDR1 
+            FROM car_car a LEFT OUTER JOIN car_car2 b ON a.CARNO = b.CARNO WHERE (b.CARNO IS NULL OR a.FINDMODE <> b.FINDMODE)"""
+    cars = Car.objects.raw(sql)
+    row_count = format(len(list(cars)), ',')
 
 
     sql = "SELECT COUNT(*) FROM car_car WHERE FINDMODE='待  尋'"
